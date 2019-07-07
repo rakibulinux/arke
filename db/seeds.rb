363b1afca36d55835c118be15821bbd089142ea0
Dima Koval <dkoval@heliostech.fr>
2019-07-07 19:46:34 +0300

    Add db:seeds task loading data from seeds.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# In the following block, I'm not using `create_or_update` method on purpose, to make the
+# command output more clear
+YAML::load_file(Rails.root.join('config', 'seeds.yml')).tap do |seeds|
+  markets = seeds.fetch('markets')
+
+  # Create or update exchanges
+  seeds.fetch('exchanges').each do |exchange_params|
+    exchange = Exchange.find_by(name: exchange_params['name'])
+
+    if exchange.nil?
+      puts "create exchange #{exchange_params['name']}: #{exchange_params}"
+      exchange = Exchange.create!(exchange_params)
+    else
+      puts "update existing exchange #{exchange.name}: #{exchange_params}"
+      exchange.update!(exchange_params)
+    end
+
+    if exchange.errors.any?
+      puts "  #{exchange.name}: error: #{exchange.errors.messages}"
+    end
+
+    # for each exchange, create markets
+    markets.each do |market_params|
+      market = Market.find_by(name: market_params['name'], exchange_id: exchange.id)
+
+      if market.nil?
+        puts "  #{exchange.name}: create market #{market_params['name']}: #{market_params}"
+        Market.create!(market_params.merge(exchange_id: exchange.id))
+      else
+        puts "  #{exchange.name}: update existing market #{market.name}: #{market_params}"
+        market.update(market_params)
+      end
+    end
+
+    puts
+  end
+end
