3e830718c14b673d969b20c845bee283069e4881
Kohelbekker <kohelbekker@gmail.com>
2019-10-14 16:23:08 +0300

    FEATURE: feature for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_many :accounts
   has_many :robots
+
+  validates :uid, presence: true, length: { maximum: 12 }, uniqueness: true
+  validates :email, presence: true, format: { with: /[\w._%+-]+@[\w.-]+\.[a-zA-z]{2,4}\Z/ }, uniqueness: true
+  validates :level, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4}
+  validates :role, presence: true, inclusion: { in: %w(admin trader broker),
+  message: "%{value} is not a valid role" }
+  validates :state, presence: true, inclusion: { in: %w(active disabled),
+  message: "%{value} is not a valid state" }
+  validates :created_at, presence: true
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,9 @@ require 'rspec/rails'
 
 require 'arke'
 
+# Clears vault, it helps when you drop your test db.
+ApiKeyService.prune!
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -65,4 +68,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 end
