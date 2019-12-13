ebaa8143fcb189a924661add8d842f924586a1e5
Denis <dfedorchenko@heliostech.fr>
2019-12-13 17:01:38 +0200

    Fix: ra-data-json-server array query bug

--- app/javascript/packs/react-admin/App.tsx
+++ app/javascript/packs/react-admin/App.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import UserIcon from '@material-ui/icons/Group';
-import jsonServerProvider from 'ra-data-json-server';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red, purple, deepPurple } from '@material-ui/core/colors';
 
+import jsonServerProvider from './data-providers/ra-data-json-server';
 import { Dashboard } from './views/dashboard';
 import { RobotList, RobotCreate, RobotEdit } from './views/robots'
 
 const dataProvider = jsonServerProvider('http://localhost:3000/api/v2/admin');
 
+const theme = createMuiTheme({
+  palette: {
+    // type: 'dark',
+    primary: purple,
+    secondary: deepPurple,
+    error: red,
+  },
+});
+
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider} theme={theme}>
     <Resource name="robots" list={RobotList} icon={UserIcon} edit={RobotEdit} create={RobotCreate} />
     <Resource name="users" list={ListGuesser} />
   </Admin>
