b2200fb1460281165946c194f87c8ca266e8a32f
Camille Meulien <camille.meulien@gmail.com>
2020-02-06 16:59:28 +0200

    Raise explicit error when driver is undefined

--- lib/arke/exchange.rb
+++ lib/arke/exchange.rb
@@ -19,6 +19,7 @@ module Arke
     # * takes +config+ (hash) and passes to Exchange initializer
     # * takes +config+ and resolves correct Exchange class with +exchange_class+ helper
     def self.create(config)
+      raise "driver is missing for account id #{config["id"]}" if config["driver"].to_s.empty?
       exchange_class(config["driver"]).new(config)
     end
 
