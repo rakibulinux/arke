188adc508de9198b6dba0bda85253cbffc365e6a
Camille Meulien <cmeulien@heliostech.fr>
2020-12-15 22:06:17 +0200

    WIP operator thread, one EM loop per strategy

--- lib/arke/command/server.md
+++ lib/arke/command/server.md
@@ -2,6 +2,16 @@
 
 ## Start a bot
 
+1. Quiet example with candle_sampling strategy
+
 ```json
 [1,42,"start_bot",{"bot_id":1,"log_level":"INFO","accounts":[{"id":"bitfaker","driver":"bitfaker","delay":1,"balances":{"btc":3,"usd":10000}}],"strategies":[{"id":"candle-FTHUSD","type":"candle_sampling","debug":true,"enabled":true,"period":1000,"params":{"sampling_ratio":100000,"max_slippage":0.005},"sources":[{"account_id":"bitfaker","market_id":"ETHUSDT"}],"target":{"account_id":"bitfaker","market_id":"ethusdt"}}]}]
 ```
+
+
+2. Example with active logs
+
+```json
+[1,42,"start_bot",{"bot_id":1,"log_level":"INFO","accounts":[{"id":"bitfaker","driver":"bitfaker","delay":1,"balances":{"btc":3,"usd":10000}}],"strategies":[{"id":"copy-ETHUSD","type":"copy","debug":true,"enabled":true,"period":30,"fx":{"type":"static","rate":1.8},"params":{"spread_bids":0.003,"spread_asks":0.003,"limit_asks_base":100,"limit_bids_base":100,"max_amount_per_order":0.5,"levels_size":1,"levels_count":10,"side":"both"},"target":{"account_id":"bitfaker","market_id":"BTCUSD"},"sources":[{"account_id":"bitfaker","market_id":"BTCUSD"}]},{"id":"microtrades-FTHUSD","type":"microtrades","debug":true,"enabled":false,"period":5,"period_random_delay":10,"params":{"min_amount":0.05,"max_amount":0.3},"target":{"account_id":"bitfaker","market_id":"BTCUSD"}}]}]
+```
+
--- lib/arke/server.rb
+++ lib/arke/server.rb
@@ -11,67 +11,98 @@ module Arke
       Arke::Log.level = "INFO"
       @logger = Arke::Log
       @bots = Concurrent::Hash.new
+      @op_q = Thread::Queue.new
     end
 
     def run
-      EM.run do
-        EM::WebSocket.run(host: @ws_host, port: @ws_port) do |ws|
-          ws.onopen do |handshake|
-            @logger.info "New websocket connection #{handshake.path}"
-            ws.send "Hello Client, you connected to #{handshake.path}"
-          end
+      @server_th = Thread.new do
+        EM.run do
+          EM::WebSocket.run(host: @ws_host, port: @ws_port) do |ws|
+            ws.onopen do |handshake|
+              @logger.info "New websocket connection #{handshake.path}"
+              ws.send "Hello Client, you connected to #{handshake.path}"
+            end
 
-          ws.onmessage {|msg| on_message(ws, msg) }
+            ws.onmessage {|msg| on_message(ws, msg) }
 
-          ws.onclose do
-            @logger.info "Websocket connection closed #{ws}"
+            ws.onclose do
+              @logger.info "Websocket connection closed #{ws}"
+            end
           end
+          @logger.info "Webosocket server listenning on #{@ws_host}:#{@ws_port}"
         end
-        @logger.info "Webosocket server listenning on #{@ws_host}:#{@ws_port}"
+      end
+
+      logger.info "Starting command loop thread"
+      loop do
+        c = @op_q.pop
+        puts "Received command #{c}"
+        logger.info "Received command #{c}"
+        type, req_id, method, params = c
+        raise "Unexpected method type" if type != 1
+
+        case method
+        when "ping"
+          respond(ws, req_id, "pong")
+        when "start_bot"
+          start_bot(req_id, ws, params)
+        when "stop_bot"
+          stop_bot(req_id, ws, params)
+        when "subscribe_logs"
+        end
+      rescue StandardError => e
+        logger.error "#{e}: #{e.backtrace.join("\n")}"
       end
     end
 
     def respond(ws, req_id, method, params=nil)
+      return
+
       ws.send [2, req_id, method, params].compact.to_json
     end
 
     def on_message(ws, msg)
-      puts "Recieved message: #{msg}"
-      type, req_id, method, params = JSON.parse(msg)
+      return if msg.strip.empty?
 
+      logger.info "Received message: #{msg}"
+      type, req_id, method, params = JSON.parse(msg)
       raise "Unexpected method type" if type != 1
 
-      case method
-      when "ping"
-        respond(ws, req_id, "pong")
-      when "start_bot"
-        start_bot(req_id, ws, params)
-      when "stop_bot"
-        stop_bot(req_id, ws, params)
-      when "subscribe_logs"
-      end
+      @op_q << [type, req_id, method, params]
+      ws.send [2, req_id, method, "msg pushed to op queue"].compact.to_json
 
     rescue StandardError => e
-      logger.error "#{e}"
+      logger.error "#{e}: #{e.backtrace.join("\n")}"
     end
 
     def start_bot(req_id, ws, params)
       bot_id = params["bot_id"]
       raise "bot_id missing" if bot_id.nil?
 
+      respond(ws, req_id, "start_bot", bots: @bots.inspect)
+
+      bot = @bots[bot_id]
+      unless bot.nil?
+        respond(ws, req_id, "start_bot", error: "bot is already running")
+        return
+      end
+
       device = ::Arke::DispatchDevice.new
       logger = Logger.new(device)
 
       th = Thread.new do
+
         Arke::Reactor.new(params["strategies"], params["accounts"], false).run
       rescue StandardError => e
-        logger.fatal("Thread received a fatal error: #{e}")
+        logger.error "Thread fatal: #{e}: #{e.backtrace.join("\n")}"
       ensure
         device.close
         @bots.delete(bot_id)
+        respond(ws, req_id, "start_bot", success: "bot ended (bot_id: #{bot_id})")
       end
 
       @bots[bot_id] = OpenStruct.new(th: th, logger: logger, device: device)
+      respond(ws, req_id, "start_bot", success: "bot started (bot_id: #{bot_id})")
     end
 
     def stop_bot(req_id, ws, _params)
